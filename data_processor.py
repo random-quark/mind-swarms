0e9261b2cb5431aa50008512c9a6062ff3645771
Tom Chambers <tom.chambers@gmail.com>
2016-09-26 14:56:06 +0100

    refine processing

--- data_processor.py
+++ data_processor.py
@@ -1,14 +1,16 @@
 import csv
 from collections import Counter
+from operator import itemgetter
 
 ranges = {
-    'sad': [[0, 1],[0, 1]],
-    'angry': [[0, 0],[0, 0]],
-    'fear': [[0, 0],[0, 0]],
-    'surprise': [[0, 0],[0, 0]],
-    'love': [[0, 0],[0, 0]],
-    'joy': [[0, 0],[0, 0]],
-    'calm': [[0, 0],[0, 0]],
+    'disgust': [[0,0.25],[0,0.5]],
+    'sad': [[0, 0.5],[0.5, 1]],
+    'anger': [[0.25, 0.5],[0.25, 0.5]],
+    'fear': [[0.25, 0.5],[0, 0.25]],
+    'surprise': [[0.5, 0.75],[0, 0.25]],
+    'love': [[0.75, 1],[0, 0.25]],
+    'joy': [[0.5, 1],[0.25, 0.5]],
+    'calm': [[0.5, 1],[0.5, 1]],
 }
 
 emotions = []
@@ -21,21 +23,20 @@ with open("data_raw/data.csv", 'r') as data_file:
         valence = float(line[1])
         activation = float(line[2])
         activations.append(activation)
+        found = False
         for emotion, parts in ranges.iteritems():
-            if parts[0][0] < valence < parts[0][1]:# and parts[1][0] < activation < parts[1][1]:
+            if parts[0][0] <= valence <= parts[0][1] and parts[1][0] <= activation <= parts[1][1]:
                 emotions.append(emotion)
-
-emotion_frequency = Counter(emotions)
+                found = emotion
 
 activation_avg = reduce(lambda x, y: x + y, activations) / len(activations)
 
-print emotion_frequency
-print activation_avg
-
-emotion_names = emotion_frequency.keys()
-print emotion_names
+emotion_frequency = Counter(emotions)
+emotions_frequency_list = [(k,v) for k,v in emotion_frequency.iteritems()]
+emotions_list_sorted = sorted(emotions_frequency_list, key=itemgetter(1), reverse=True)
+print emotions_list_sorted
 
 writer = csv.writer(open("data_processed/data.csv", 'w'))
-for e in emotion_names:
-    writer.writerow([e])
+for e in emotions_list_sorted:
+    writer.writerow([e[0]])
 writer.writerow([activation_avg])
