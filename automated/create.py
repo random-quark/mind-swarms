cc1635b5f2f059dc8bf74c838de5e22c2940cf80
Tom Chambers <tom.chambers@gmail.com>
2016-10-14 19:05:38 +0100

    fix up automation

--- automated/create.py
+++ automated/create.py
@@ -8,19 +8,21 @@ with open('thoughts.csv', "r") as data_file:
     for line in reader:
         if (line[0] not in people):
             people[line[0]] = []
-        people[line[0]].append((line[1], line[2], line[3]))
+        people[line[0]].append({
+            "title": line[1],
+            "emotion1": { "name": line[2], "proportion": line[4] },
+            "emotion2": { "name": line[3], "proportion": line[5] },
+            "activation_average": line[6]
+        })
 
 for person, thoughts in people.iteritems():
     for thought in thoughts:
         dataFile = open("viz.app/Contents/Java/data/data.csv", "w")
         writer = csv.writer(dataFile)
-        thought_name = thought[0]
-        emotion1 = thought[1]
-        emotion2 = thought[2]
-        writer.writerow([emotion1, 0.5])
-        writer.writerow([emotion2, 0.5])
-        writer.writerow(['0.5'])
-        writer.writerow([thought_name])
+        writer.writerow([thought["emotion1"]["name"], thought["emotion1"]["proportion"]])
+        writer.writerow([thought["emotion2"]["name"], thought["emotion2"]["proportion"]])
+        writer.writerow([thought["activation_average"]])
+        writer.writerow([thought["title"]])
         writer.writerow([person])
         dataFile.flush()
         subprocess.call(["open", "-W", "viz.app"])
